762338a80462060f8d90114535d5bd697b891864
Aayush Chadha <achadharocks@gmail.com>
2019-03-16 19:00:21 +0000

    Add home page template

---
+++ khan-academy-netflix/src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="main">
+   <div class="col-md-12">
+
+       <h1 class="title-text col-md-12"> KAn't Netflix and Chill </h1>
+       <!-- TODO Add Subtitle -->
+       <!--<h3 class="subtitle-text col-md-12">Looking for a film to watch with your SO?-->
+           <!--How about you expand the horizons of your mind with Khan Academy?</h3>-->
+
+   </div>
+  </div>
+</template>
+
+<script>
+
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+
+Vue.use(VueMaterial);
+
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
